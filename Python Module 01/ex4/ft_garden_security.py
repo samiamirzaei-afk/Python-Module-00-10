b678a58d147314e632b515d86a1ee17224c000ae
Sony <ammirzae@student.42vienna.com>
2026-08-08 17:39:53 +0200

    [V0.3] added ex4, ex5 WIP

--- Python Module 01/ex4/ft_garden_security.py
+++ Python Module 01/ex4/ft_garden_security.py
@@ -1,47 +1,61 @@
 class Plant:
     def __init__(self, name: str, height: float, old: int,
-                 growth_multi : float) -> None:
+                 growth_multi: float) -> None:
         self._name = name.capitalize()
         self._height = 0.0
         self._old = 0
-        self.set_height(height)
-        self.set_age(old)
+        self.set_height_start(height)
+        self.set_age_start(old)
         self._growth_multi = growth_multi
 
     def get_age(self) -> int:
-        pub_old = self._old 
+        pub_old = self._old
         return(pub_old)
 
-    def set_age(self, num: int) -> int:
+    def set_age(self, num: int) -> None:
         if(num < 0):
             print(f"{self._name}: Error, age can not be a negative")
-            print(f"Age updated rejected")
+            print("Age updated rejected")
+            return
+        self._old = num
+        print("Age updated to %d days old" % (self._old))
+
+    def set_age_start(self, num: int) -> None:
+        if(num < 0):
+            print(f"{self._name}: Error, age can not be a negative")
+            print("Age updated rejected")
             return
         self._old = num
 
-    def get_height(self) -> int:
+    def get_height(self) -> float:
         pub_height = self._height
         return(pub_height)
 
-    def set_height(self, num: float) -> int:
+    def set_height(self, num: float) -> None:
         if(num < 0):
             print(f"{self._name}: Error, height can not be a negative")
-            print(f"height updated rejected")
+            print("height updated rejected")
+            return
+        self._height = num
+        print("Height updated to %.2fcm" % (self._height))
+
+    def set_height_start(self, num: float) -> None:
+        if(num < 0):
+            print(f"{self._name}: Error, height can not be a negative")
+            print("height updated rejected")
             return
         self._height = num
 
-
-
     def show(self) -> None:
-            print("%s: %.2fcm, %d days old" % (
-                self._name, self._height, self._old))
+        print("%s: %.2fcm, %d days old" % (
+           self._name, self._height, self._old))
 
     def grow(self) -> None:
         self._height *= self._growth_multi
 
     def age(self) -> None:
-        self._old += 1 
-     
+        self._old += 1
+
 
 # name: str, height: float-cm, old: int-days, growth_multi : float) -> None:
 if __name__ == "__main__":
@@ -50,12 +64,15 @@ if __name__ == "__main__":
     p3 = Plant("jalapeno", 40.0, 30, 1.12)
     p4 = Plant("barley ", 5.0, 7, 1.07)
     p5 = Plant("garlic", 3.0, 12, 1.02)
-    
-    p1.show()
 
-    p1.set_age(-5)
-    p1.set_age(20)
-    p1.show()
-    p1.set_height(-5)
+    print("PLant created: %s: %.2fcm %d days old\n" % (
+        p1._name, p1._height, p1._old))
     p1.set_height(20)
-    p1.show()
+    p1.set_age(20)
+    print("")
+    p1.set_age(-5)
+    p1.set_height(-5)
+    print("")
+
+    print("Current state: %s: %.2fcm %d days old" % (
+        p1._name, p1._height, p1._old))
---
+++ Python Module 01/ex5/ft_plant_types.py
@@ -0,0 +1,122 @@
+class Plant:
+    def __init__(self, name: str, height: float, old: int,
+                 growth_multi: float) -> None:
+        self._name = name.capitalize()
+        self._height = 0.0
+        self._old = 0
+        self.set_height_start(height)
+        self.set_age_start(old)
+        self._growth_multi = growth_multi
+
+    def get_age(self) -> int:
+        pub_old = self._old
+        return(pub_old)
+
+    def set_age(self, num: int) -> None:
+        if(num < 0):
+            print(f"{self._name}: Error, age can not be a negative")
+            print("Age updated rejected")
+            return
+        self._old = num
+        print("Age updated to %d days old" % (self._old))
+
+    def set_age_start(self, num: int) -> None:
+        if(num < 0):
+            print(f"{self._name}: Error, age can not be a negative")
+            print("Age updated rejected")
+            return
+        self._old = num
+
+    def get_height(self) -> float:
+        pub_height = self._height
+        return(pub_height)
+
+    def set_height(self, num: float) -> None:
+        if(num < 0):
+            print(f"{self._name}: Error, height can not be a negative")
+            print("height updated rejected")
+            return
+        self._height = num
+        print("Height updated to %.2fcm" % (self._height))
+
+    def set_height_start(self, num: float) -> None:
+        if(num < 0):
+            print(f"{self._name}: Error, height can not be a negative")
+            print("height updated rejected")
+            return
+        self._height = num
+
+    def show(self) -> None:
+        print("%s: %.2fcm, %d days old" % (
+           self._name, self._height, self._old))
+
+    def grow(self) -> None:
+        self._height *= self._growth_multi
+
+    def age(self) -> None:
+        self._old += 1
+
+class Flower(Plant):
+    def __init__(self, name: str, height: float, old: int,
+            growth_multi: float, color: str) -> None:
+        super().__init__(name, height, old, growth_multi)
+        self._color = color
+
+    def bloom(self) -> None:
+        if(self._old < 20):
+            print("%s is too young to bloom!, needs %d days" % (
+                self._name ,20 - self._old))
+        else:
+            print("%s is blooming %s" % (self._name, self._color))
+
+    def show(self) -> None:
+        super().show()
+        print("Color: %s" % (self._color))
+    
+Class Tree(Plant):
+    def __init__(self, name: str, height: float, old: int,
+            growth_multi: float, trunk_diameter: float) -> None:
+        super().__init__(name, height, old, growth_multi)
+        self._trunk_diameter = trunk_diameter
+
+    def show(self) -> None:
+        super().show()
+        print("Trunk diameter: %.2fcm" % (trunk_diameter))
+
+    def produce_shade(self):
+        print("%s creates a shadow of %.2fcm², (h:%.2f, w:%.2f)" % (
+            self._name, (self._height * self._trunk_diameter), self._height,
+            self._trunk_diameter))
+
+class Vegetable(Plant):
+    def __init__(self, name: str, height: float, old: int,
+            growth_multi: float, harvest_season: str) -> None:
+        super().__init__(name, height, old, growth_multi)
+        self._harvest_season = harvest_season
+        self._nutritional_value = 0
+
+    def show(self) -> None:
+        super().show()
+        printf("Harevest season: %s)" % (self._harvest_season)
+        printf("Nutritional value: %d)" % (self._nutritional_value)
+
+    def age(self) -> None:
+        super().age()
+        self._nutritional_value += 1
+
+    def grow(self) -> None:
+        super().age()
+        self._nutritional_value += 1
+
+# name: str, height: float-cm, old: int-days, growth_multi : float) -> None:
+if __name__ == "__main__":
+    p1 = Flower("rosemary", 10.0, 5, 1.04, "purple")
+    p2 = Tree
+    p1.show()
+    p1.bloom()
+    x = 1
+    for x in range(x , 30):
+        p1.age()
+        p1.bloom()
+
+
