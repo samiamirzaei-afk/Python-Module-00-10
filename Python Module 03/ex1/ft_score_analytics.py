f2e2199de29f62ccc44479c0c97b9ba2c6fde119
Sony <ammirzae@student.42vienna.com>
2026-08-24 13:17:17 +0200

    [V1.0] ex2 has a simpler version now

--- Python Module 03/ex1/ft_score_analytics.py
+++ Python Module 03/ex1/ft_score_analytics.py
@@ -2,10 +2,11 @@ import sys
 
 def is_number(value: int) -> bool:
     try:
-        value += 1
+        _ = int(value)
         return True
     except(TypeError, ValueError):
         return False
+
 def main(argc: int) -> int:
     if(argc < 2):
         print("No score given, Use: python3.11 ft_score_analytics.py score1 score2 score3...")
@@ -14,33 +15,29 @@ def main(argc: int) -> int:
     argv = sys.argv
     score_list = []
     j = 1
-    k = 0
-    tester = 100
 
-    print(f"{len(argv[1])}")
-    print(f"{len(argv[2])}")
-    print(f"{len(argv[3])}")
-    print(f"{len(argv[4])}")
-    print(f"{len(argv[5])}")
     while(j < argc):
         if is_number(argv[j]) is True:
-            score_list.append(argv[j])
+            score_list.append(int(argv[j]))
         else:
             print(f"{argv[j]} is not a number")
         j += 1
-    
     print(score_list)
-    return(1);
+    print(f"total players: {len(score_list)}")
+    print(f"total score: {sum(score_list)}")
+    print(f"highest score: {max(score_list)}")
+    print(f"smallest score: {min(score_list)}")
+    print(f"avrage score: {(sum(score_list) / len(score_list))}")
+    print(f"range score: {(max(score_list) - min(score_list))}")
 
-    i = 1
-    all_scores = ""
-    while(i < argc):
-        all_scores += argv[i]
-        i += 1
-        if(i < argc):
-            all_scores += ", "
-    print(f"[{all_scores}]")
 
+
+
+    ''' 
+    temp = score_list.__sizeof__()
+    print(score_list)
+    print(f"{temp}")
+    '''
 if __name__ == "__main__":
     argc = len(sys.argv)
     main(argc)
