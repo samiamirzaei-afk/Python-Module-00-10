f7855b9a5669a84e6a18069b52531b977e1c6bf4
Study-lap <study-lap@studylap-Aspire-ES1-571.me>
2026-09-15 13:01:02 +0200

    [V1.9] P04: ex3 done

--- Python Module 04/ex3/ft_stream_management.py
+++ Python Module 04/ex3/ft_stream_management.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def argc_check(argc: int) -> bool:
     if (argc == 1):
         print("please provide arguments (python ft_ancient_text.py file.txt)")
@@ -10,50 +11,32 @@ def argc_check(argc: int) -> bool:
     return True
 
 
-def read_n_clone(full_lines: list[str]) -> list[str]:
-    i = 0
-    for line in full_lines:
-        new_line = line.replace("\n", "#\n")
-        print(line, end="")
-        full_lines[i] = new_line
-        i += 1
-    print("done!")
-    print("now cloning...\n")
-    for line in full_lines:
-        print(line, end="")
-    return (full_lines)
+def secure_archive(argv: str, option: int) -> tuple[bool, str]:
+    mode = "r" if option == 1 else "w"
+    try:
+        with open(argv) as f:
+            content = f.read()
+            if mode == "w":
+                with open("archive.txt", mode) as f2:
+                    f2.write(content)
+                    f2.write("###archive text###")
+                    return(True, "content wrote in file \"archive.txt\"")
+            return(True, content)
+    except (FileNotFoundError, PermissionError) as e:
+        return(False, str(e))
+
 
 def main() -> int:
+    read = 1
+    write = 2
     argc = len(sys.argv)
     if not argc_check(argc):
         return (1)
-    try:
-        f = open(sys.argv[1])
-    except (PermissionError, FileNotFoundError) as e:
-        print(e, file=sys.stderr)
-        return (1)
-
-    print("reading...")
-    full_lines = f.readlines()
-    read_n_clone(full_lines)
-    print("\ndone!")
-    f.close()
-    print("enter file name:(leave empty to ignore): ", end="")
-    sys.stdout.flush()
-    save_name = sys.stdin.readline().rstrip("\n")
-    if save_name == "":
-        print("no name was given, deleting file")
-        return (0)
-    try:
-        temp = open(save_name, 'w')
-        for line in full_lines:
-            temp.write(line)
-    except (PermissionError) as e:
-        print(e, file=sys.stderr)
-    print("file saved as", save_name)
-    temp.close()
+    result = secure_archive(sys.argv[1], read)
+    print(result)
+    result = secure_archive(sys.argv[1], write)
+    print(result)
     return (0)
-
 
 
 if __name__ == "__main__":
